1438b90beb7f49deec5ad864cd00b87a9e50e255
Illia Kravchenko <il.kravchenko115@gmail.com>
2023-10-29 16:18:41 +0300

    Add strings.xml for Ukraine users and Update @Preview

---
+++ app/src/main/java/com/example/lab1/TranslationPreview.kt
@@ -0,0 +1,15 @@
+package com.example.lab1
+
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(
+    name = "English",
+    group = "locale",
+    locale = "en"
+)
+@Preview(
+    name = "Ukraininan",
+    group = "locale",
+    locale = "uk"
+)
+annotation class TranslationPreview
